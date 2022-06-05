b293728a1ded387ccee885f331c594902accfbfd
chuongtang <chuong.tang@aol.com>
2022-06-05 14:32:40 -0600

    commit to swtich: check metamask in useEffect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {
-  Text, Alert, AlertIcon, AlertTitle, AlertDescription, Heading
+  Text, Alert, AlertIcon, Heading, Button
 } from '@chakra-ui/react'
 import { ethers } from "ethers";
 import { MetaMaskInpageProvider } from "@metamask/providers";
@@ -11,14 +11,15 @@ import { MetaMaskInpageProvider } from "@metamask/providers";
 // to fix "type not exist" error
 declare global {
   interface Window {
-    ethereum?: MetaMaskInpageProvider
+    ethereum: MetaMaskInpageProvider
   }
 }
 
-const App = () => {
-
+const App: React.FC = () => {
+  const [currentAccount, setCurrentAccount] = useState('');
   const [message, setMessage] = useState('')
-  const checkIfWalletIsConnected = () => {
+
+  const checkIfWalletIsConnected = async () => {
     // First make sure we have access to window.ethereum
     const { ethereum } = window;
 
@@ -29,28 +30,43 @@ const App = () => {
     } else {
       console.log("We have the ethereum object", ethereum);
     }
-  }
+
+    // Check if we're authorized to access the user's wallet
+    let accounts : (string | number)[] =  await ethereum.request({ method: 'eth_accounts' });
+
+    // Users can have multiple authorized accounts, we grab the first one if its there!
+    if (accounts.length !== 0) {
+      let account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
 
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
 
+  // Connect wallet to app
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+      <img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja gif" />
+      <Button colorScheme='orange'>Connect your wallet</Button>
+    </div>
+  );
+
   return (
     <div className="App">
+      {message !== '' &&
+        <Alert status='error'>
+          <AlertIcon />
+          {message}
+        </Alert>}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Heading as='h1' size='4xl'>Name Service</Heading>
-        {message !== '' &&
-          <Alert status='error'
-            flexDirection='column'
-            alignItems='center'
-            justifyContent='center'
-            textAlign='center'
-            >
-            <AlertIcon />
-            <AlertTitle>Error</AlertTitle>
-            <AlertDescription>{message}</AlertDescription>
-          </Alert>}
         <Text m={3} borderRadius={'lg'} p={4} color='gray.500' bgGradient='linear(to-r, gray.300, yellow.400, pink.200)' noOfLines={1}>
           Register a domain on Polygon blockchain
         </Text>
